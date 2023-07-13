7fd9d68342de4edb91e657e98bf92de040ba6441
junsantilla <junsantilla@live.com>
2023-07-13 22:24:00 +0200

    Add error and exception handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,26 @@
-const registerUser = (req, res) => {
-    res.send('Register Router')
-}
+const asyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+// @desc        Register a new user
+// @route       /api/users
+// @access      public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    // Validation
+    if (!name || !email || !password) {
+        es.status(400)
+        throw new Error('Please include all fields')
+    }
+
+    res.send('Register Router')
+})
+
+// @desc        Login a user
+// @route       /api/login
+// @access      public
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Router')
-}
+})
 
 module.exports = {
     registerUser,
